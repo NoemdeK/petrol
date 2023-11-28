b08c355f99b05e88de8bdcc41491f51ee6a99d14
Phenoo <phenopreneur@outlook.com>
2023-11-28 19:04:12 +0100

    sixtth

--- app/page.tsx
+++ app/page.tsx
@@ -16,6 +16,7 @@ import {
 } from "@/components/ui/navigation-menu"
 import React from 'react'
 import { cn } from '@/lib/utils'
+import Canvas from '@/components/Canvas'
 
 export default function Home() {
   const components: { title: string; href: string; description: string }[] = [
@@ -40,56 +41,65 @@ export default function Home() {
 
   ]
   return (
-    <main className="flex min-h-screen flex-col justify-center  gap-12 p-12 ">
-      <div className="z-10 w-full items-center justify-between lg:flex">
-        <p className=" left-0 top-0 flex w-full justify-center text-xl md:text-5xl pb-6 pt-8 text-sky-400  lg:static lg:w-auto   lg:p-4 ">
-          dio<span className='font-bold'>phalytics</span><span className='text-black'>.io</span>
-        </p>
+    <main className='min-h-screen h-full flex-col flex'>
+        <div className="z-10 w-full items-center justify-between flex p-4 md:p-8">
+          <p className=" left-0 top-0 flex w-full text-2xl md:text-5xl pb-6 pt-8 text-sky-400  lg:static lg:w-auto   lg:p-4 ">
+            dio<span className='font-bold'>phalytics</span><span className='text-black'>.io</span>
+          </p>
 
-        <div className=" bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
+          <div className="w-fit items-end justify-end bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
 
-              <NavigationMenu>
-          <NavigationMenuList>
+                <NavigationMenu>
+            <NavigationMenuList>
 
-            <NavigationMenuItem>
-              <NavigationMenuTrigger>About Us</NavigationMenuTrigger>
-              <NavigationMenuContent>
-                <ul className="grid gap-3 p-2 w-36">
-                  {components.map((component) => (
-                    <ListItem
-                      key={component.title}
-                      title={component.title}
-                      href={component.href}
-                    >
-                      {component.description}
-                    </ListItem>
-                  ))}
-                </ul>
-              </NavigationMenuContent>
-            </NavigationMenuItem>
-           
-          </NavigationMenuList>
-        </NavigationMenu>
-        </div>
-      </div>
-
-     <div className='h-full flex items-center'>
-      <div className='h-full flex flex-col gap-8 mt-16 px-4'>
-          <h4 className='text-xl sm:text-3xl md:text-5xl lg:text-6xl leading-loose font-bold'>
-            Solving the <span className='text-purple-800'>hardest</span> <br />
-            data collection and <br />
-            veification challenges
-          </h4>
-          <div className='flex items-center gap-4'>
-            <p className='text-lg md:text-xl'>Check out our tools</p>
-            <Link href={'/signin'} className='bg-white p-2 border rounded-md'>
-              <Logo />
-            </Link>
+              <NavigationMenuItem>
+                <NavigationMenuTrigger>About Us</NavigationMenuTrigger>
+                <NavigationMenuContent>
+                  <ul className="grid gap-3 p-2 w-36">
+                    {components.map((component) => (
+                      <ListItem
+                        key={component.title}
+                        title={component.title}
+                        href={component.href}
+                      >
+                        {component.description}
+                      </ListItem>
+                    ))}
+                  </ul>
+                </NavigationMenuContent>
+              </NavigationMenuItem>
+            
+            </NavigationMenuList>
+          </NavigationMenu>
           </div>
         </div>
-     </div>
+      <div className="flex h-full flex-col justify-center w-full md:items-start gap-12 p-4 md:p-12 ">
+        <div className="grid grid-cols-1 lg:grid-cols-2 gap-4">
 
+          <div className='h-full flex items-center flex-col justify-center text-center md:text-left md:items-start'>
+            <div className='h-full flex flex-col gap-8 mt-16 px-4 justify-center items-center md:items-start'>
+                <h4 className='text-4xl sm:text-5xl lg:text-6xl font-bold '>
+                  Solving the <span className='text-purple-800'>hardest</span> <br />
+                  data collection and 
+                  veification challenges
+                </h4>
+                <div className='flex items-center gap-4'>
+                  <p className='text-lg md:text-xl'>Check out our tools</p>
+                  <Link href={'/signin'} className='bg-white p-2 border rounded-md'>
+                    <Logo />
+                  </Link>
+                </div>
+              </div>
+          </div>
+          <div className='hidden lg:flex w-full '>
+            <Canvas />
+          </div>
+        </div>
+      
+
+      </div>
     </main>
+
   )
 }
 
---
+++ components/Canvas.tsx
@@ -0,0 +1,42 @@
+
+import React, { Component } from 'react';
+//@ts-ignore
+import CanvasJSReact from '@canvasjs/react-charts';
+//var CanvasJSReact = require('@canvasjs/react-charts');
+ 
+;
+
+const CanvasJS = CanvasJSReact.CanvasJS;
+const CanvasJSChart = CanvasJSReact.CanvasJSChart;
+
+const Canvas = () => {
+  const options = {
+    animationEnabled: true,
+    exportEnabled: true,
+    theme: "dark2", // "light1", "dark1", "dark2"
+    title: {
+      text: "PMS Expenses"
+    },
+    data: [{
+      type: "pie",
+      indexLabel: "{label}: {y}%",
+      startAngle: -90,
+      dataPoints: [
+        { y: 20, label: "Oil" },
+        { y: 24, label: "Petroleum Gas" },
+        { y: 20, label: "Kerosene" },
+        { y: 14, label: "Diesel" },
+        { y: 12, label: "ICE Brent" },
+        { y: 10, label: "Aviation Fuel" }
+      ]
+    }]
+  };
+
+  return (
+    <div>
+      <CanvasJSChart options={options} />
+    </div>
+  );
+};
+
+export default Canvas;
