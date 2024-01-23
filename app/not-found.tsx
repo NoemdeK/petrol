ed1e85a11ac59c50ca0480fb1cf3c2445338e1f5
Phenoo <phenopreneur@outlook.com>
2024-01-23 22:20:43 +0100

    build push for presentation

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const notfound = () => {
   return (
     <div className="absolute inset-0 flex flex-col items-center justify-center">
-      <h2 className="text-xl font-bold">{`Couldn't find requested page`}</h2>
+      <h2 className="text-xl font-bold">{`Couldn't find requested page!`}</h2>
      <Back />
     </div>  )
 }
