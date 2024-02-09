307b4c16bdfa48fa4dc18919fbe347a7fb1deefa
emmanueldek <eowolabi@noemdek.co.uk>
2024-02-09 12:51:41 +0100

    Fixed nav

--- components/NavigationBar.tsx
+++ components/NavigationBar.tsx
@@ -1,85 +1,87 @@
-"use client"
+"use client";
 
 import {
-    NavigationMenu,
-    NavigationMenuContent,
-    NavigationMenuItem,
-    NavigationMenuLink,
-    NavigationMenuList,
-    NavigationMenuTrigger,
-    navigationMenuTriggerStyle,
-  } from "@/components/ui/navigation-menu"
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu";
 import { cn } from "@/lib/utils";
 import { usePathname } from "next/navigation";
 import React from "react";
 
 export default function NavigationBar() {
-    const components: { title: string; href: string; description: string }[] = [
-        {
-        title: "Home",
-        href: "/",
-        description:
-            "A modal dialog that interrupts the user with important content and expects a response.",
-        },
-        {
-        title: "Our Process",
-        href: "/process",
-        description:
-            "For sighted users to preview content available behind a link.",
-        },
-        {
-        title: "Contact Us",
-        href: "/contact",
-        description:
-            "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
-        },
+  const components: { title: string; href: string; description: string }[] = [
+    {
+      title: "Home",
+      href: "/",
+      description:
+        "A modal dialog that interrupts the user with important content and expects a response.",
+    },
+    {
+      title: "Our Process",
+      href: "/process",
+      description:
+        "For sighted users to preview content available behind a link.",
+    },
+    {
+      title: "Contact Us",
+      href: "/contact",
+      description:
+        "Displays an indicator showing the completion progress of a task, typically displayed as a progress bar.",
+    },
+  ];
 
-    ]
+  const pathname = usePathname();
 
-    const pathname = usePathname()
-    
-    let trigger = "Home " 
+  let trigger = "Home";
 
-    switch(pathname) {
-      case "/process":
-        trigger = "Our Process";
+  switch (pathname) {
+    case "/process":
+      trigger = "Our Process";
       break;
-      case "/contact":
-        trigger = "Contact Us";
+    case "/contact":
+      trigger = "Contact Us";
       break;
-      case "/":
-        trigger = "Home";
+    case "/":
+      trigger = "Home";
       break;
-        default:
-          trigger = "Home"
-    }
-    
-  return(
-    <NavigationMenu>
-    <NavigationMenuList>
-      <NavigationMenuItem>
-        <NavigationMenuTrigger className="text-black">
-
-          {trigger}
-        </NavigationMenuTrigger>
-        <NavigationMenuContent>
-          <ul className="grid gap-3 p-2 w-36">
-            {components.map((component) => (
-              <ListItem
-                key={component.title}
-                title={component.title}
-                href={component.href}
-              >
-                {component.description}
-              </ListItem>
-            ))}
-          </ul>
-        </NavigationMenuContent>
-      </NavigationMenuItem>
-    </NavigationMenuList>
-  </NavigationMenu>
-  )
+    default:
+      trigger = "Home";
   }
+
+  return (
+    <NavigationMenu>
+      <NavigationMenuList>
+        <NavigationMenuItem>
+          <NavigationMenuTrigger className="text-black">
+            {pathname === "/" ||
+            pathname === "/process" ||
+            pathname === "/contact"
+              ? trigger
+              : "Diophalytics"}
+          </NavigationMenuTrigger>
+          <NavigationMenuContent>
+            <ul className="grid gap-3 p-2 w-36">
+              {components.map((component) => (
+                <ListItem
+                  key={component.title}
+                  title={component.title}
+                  href={component.href}
+                >
+                  {component.description}
+                </ListItem>
+              ))}
+            </ul>
+          </NavigationMenuContent>
+        </NavigationMenuItem>
+      </NavigationMenuList>
+    </NavigationMenu>
+  );
+}
 
 const ListItem = React.forwardRef<
   React.ElementRef<"a">,
@@ -100,6 +102,6 @@ const ListItem = React.forwardRef<
         </a>
       </NavigationMenuLink>
     </li>
-  )
-})
-ListItem.displayName = "ListItem"
+  );
+});
+ListItem.displayName = "ListItem";
